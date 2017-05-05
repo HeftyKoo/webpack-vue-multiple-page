01c3c5ef871731cfacd3bfbf9902e808485e936f
yeyuqiudeng <yeyuqiudeng@gmail.com>
2017-05-05 22:34:13 +0800

    add example m

---
+++ src/m/views/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>views</p>
+</template>
+
+<style type="text/less" lang="less" scoped>
+  @import "~@ma/assets/style/_variable.less";
+</style>
+
+<script type="text/babel">
+  export default {
+     
+  }
+</script>
+
